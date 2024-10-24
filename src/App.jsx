07cc4a0aaffa70c59e1480a3d6ad2b616370d390
itsmearun1 <arundevvb1@gmail.com>
2024-10-24 23:28:53 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useState } from "react";
 import Draggable from "react-draggable";
 import "./App.css";
+
 function App() {
   const [task, setTask] = useState({
     todo: [],
@@ -12,13 +13,36 @@ function App() {
   });
   const [input, setInput] = useState("");
   const addTodo = () => {
+    if(input.trim()==="") return;
     setTask((prevTask) => ({
       ...prevTask,
       todo: [...prevTask.todo, input],
     }));
     setInput("");
   };
-  return <div>App</div>;
+  return <div className="App">
+    <h1>TODO LIST</h1>
+    <div className="addTask">    
+      <input type="text" value={input} onChange={(e) => setInput(e.target.value)} placeholder="Add A New Task"/>
+      <button onClick={addTodo}>Add</button>
+    </div>
+    <div className="taskColumns">
+      {["todo","in Progress","done","closed","review"].map((s,i)=>(
+        <div className="taskColumn">
+          <h2>{s.toUpperCase()}</h2>
+          {
+            s==="todo" && task["todo"].map((t,ti)=>(
+              <Draggable>
+                <div className="taskItem" key={ti}>
+                  {t}
+                </div>
+              </Draggable>
+            ))
+          }
+        </div>
+      ))}
+    </div>
+  </div>;
 }
 
 export default App;
